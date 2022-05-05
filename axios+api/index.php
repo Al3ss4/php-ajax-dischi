73ee276dcd0d4437710bf5799be4804057f5f7ac
Mario Francesco Rovito <alessandro.r14@gmail.com>
2022-05-05 21:15:28 +0200

    add file for milestone 2

--- axios+api/index.php
+++ axios+api/index.php
@@ -1,10 +1,4 @@
-<!-- Prima Milestone: Stampiamo i dischi solo con l’utilizzo di PHP, che stampa direttamente i dischi in pagina: al caricamento della pagina ci saranno tutti i dischi. -->
 
-<?php
-
-include __DIR__ . '../database/db.php';
-
-?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -13,6 +7,8 @@ include __DIR__ . '../database/db.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js" integrity="sha512-bZS47S7sPOxkjU/4Bt0zrhEtWx0y0CRkhEp8IckzK+ltifIIE9EMIMTuT/mEzoIMewUINruDBIR/jJnbguonqQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>php-ajax-dischi</title>
 </head>
